f72d02eb735b29271916de0d3466abaf1185826b
mojibhashemi123 <muha006@student.kristiania.no>
2021-11-03 01:17:21 +0100

    Added HttpServer - arbeidskrav 2

---
+++ AvansertJavaEksamen/src/main/java/no/kristiania/http/HttpServer.java
@@ -0,0 +1,136 @@
+package no.kristiania.http;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class HttpServer {
+
+    private final ServerSocket serverSocket;
+    private Path rootDirectory;
+    private List<String> roles = new ArrayList<>();
+
+
+    public HttpServer(int serverPort) throws IOException {
+
+        serverSocket = new ServerSocket(serverPort);
+
+        new Thread(this::handleClients).start();
+    }
+
+    private void handleClients () {
+        try {
+            while (true){
+                handleClient();
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void handleClient() throws IOException {
+        Socket clientSocket = serverSocket.accept();
+
+        HttpMessage httpMessage = new HttpMessage(clientSocket);
+        String[] requestLine = httpMessage.startLine.split(" ");
+        String requestTarget = requestLine[1];
+
+        if (requestTarget.equals("/api/questions")){
+
+            String responseText = null;
+            if (responseText == null){
+                responseText = "<h3>List is empty</h3>";
+            }
+
+            writeOkResponse(clientSocket, responseText, "text/html");
+        }
+
+        if (requestTarget.equals("/hello")){
+
+            String responseText = "Hello world";
+            writeOkResponse(clientSocket, responseText, "text/html");
+        }
+
+        else if (requestTarget.equals("/shouldReturnContent")){
+
+            String responseText = "<p>Hello world</p>";
+            writeOkResponse(clientSocket, responseText, "text/html");
+        }
+        else {
+
+            if (rootDirectory != null && Files.exists(rootDirectory.resolve(requestTarget.substring(1)))){
+
+                String responseText = Files.readString(rootDirectory.resolve(requestTarget.substring(1)));
+
+                String contentType = "text/plain";
+                if (requestTarget.endsWith(".html")){
+                    contentType = "text/html";
+                }
+                else if (requestTarget.endsWith(".css")){
+                    contentType = "text/css";
+                }
+
+                writeOkResponse(clientSocket, responseText, contentType);
+                return;
+            }
+
+            String responseText = "File not found: " + requestTarget;
+
+            String response = "HTTP/1.1 404 Not found\r\n" +
+                    "Content-Length: " + responseText.length() + "\r\n" +
+                    "Connection: close\r\n" +
+                    "\r\n" +
+                    responseText;
+            clientSocket.getOutputStream().write(response.getBytes());
+        }
+    }
+
+    private Map<String, String> parseRequestParameters(String query) {
+        Map<String, String> queryMap = new HashMap<>();
+        for (String queryParameter : query.split("&")) {
+            int equalsPos = queryParameter.indexOf('=');
+            String parameterName = queryParameter.substring(0, equalsPos);
+            String parameterValue = queryParameter.substring(equalsPos+1);
+            queryMap.put(parameterName, parameterValue);
+        }
+        return queryMap;
+    }
+
+    private void writeOkResponse(Socket clientSocket, String responseText, String contentType) throws IOException {
+        String response = "HTTP/1.1 200 OK\r\n" +
+                "Content-Length: " + responseText.getBytes().length + "\r\n" +
+                "Content-Type: " + contentType + "\r\n" +
+                "Connection: close\r\n" +
+                "\r\n" +
+                responseText;
+        clientSocket.getOutputStream().write(response.getBytes());
+    }
+
+
+    public static void main(String[] args) throws IOException {
+        HttpServer httpServer = new HttpServer(8000);
+        httpServer.setRoot(Paths.get("AvansertJavaEksamen/src/main/resources"));
+
+    }
+
+    public int getPort() {
+        return serverSocket.getLocalPort();
+    }
+
+    public void setRoot(Path RootDirectory) {
+        this.rootDirectory = RootDirectory;
+    }
+
+    public void setRoles(List<String> roles) {
+        this.roles = roles;
+    }
+
+}
---
+++ AvansertJavaEksamen/src/test/java/no/kristiania/http/HttpServerTest.java
@@ -0,0 +1,87 @@
+package no.kristiania.http;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.time.LocalTime;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HttpServerTest {
+
+    private final HttpServer server = new HttpServer(0);
+
+    HttpServerTest() throws IOException {
+    }
+
+    @Test
+    void shouldReturn404ForUnknowRequestTarget() throws IOException {
+        HttpClient client = new HttpClient("localhost", server.getPort(), "/non-existing");
+        assertEquals(404, client.getStatusCode());
+    }
+
+    @Test
+    void shouldResponseWithRequestTargetIn404() throws IOException {
+        HttpClient client = new HttpClient("localhost", server.getPort(), "/non-existing");
+        assertEquals("File not found: /non-existing", client.getMessageBody());
+    }
+
+    @Test
+    void shouldRespondWith200ForKnownRequestTarget() throws IOException {
+        HttpClient client = new HttpClient("localhost", server.getPort(), "/hello");
+        assertAll(
+                () -> assertEquals(200, client.getStatusCode()),
+                () -> assertEquals("Hello world", client.getMessageBody())
+        );
+    }
+    @Test
+    void shouldReturnContentType200() throws IOException {
+        HttpClient client = new HttpClient("localhost", server.getPort(), "/shouldReturnContent");
+        assertAll(
+                () -> assertEquals(200, client.getStatusCode()),
+                () -> assertEquals("text/html", client.getHeader("Content-Type")),
+                () -> assertEquals("<p>Hello world</p>", client.getMessageBody())
+        );
+    }
+
+    @Test
+    void shouldServeFiles() throws IOException {
+        server.setRoot(Paths.get("target/test-classes"));
+
+        String fileContent = "A file created at " + LocalTime.now();
+        Files.write(Paths.get("target/test-classes/example-file.txt"), fileContent.getBytes());
+
+        HttpClient client = new HttpClient("localhost", server.getPort(), "/example-file.txt");
+
+        assertEquals(fileContent, client.getMessageBody());
+        assertEquals("text/plain", client.getHeader("Content-Type"));
+    }
+
+
+    @Test
+    void shouldUseFileExtensionForContentType() throws IOException {
+
+        server.setRoot(Paths.get("target/test-classes"));
+
+        String fileContent = "<p>Hello world</p>";
+        Files.write(Paths.get("target/test-classes/example-file.html"), fileContent.getBytes());
+
+        HttpClient client = new HttpClient("localhost", server.getPort(), "/example-file.html");
+        assertEquals("text/html", client.getHeader("Content-Type"));
+
+    }
+
+    @Test
+    void shouldHandleMoreThanOneRequest() throws IOException {
+        assertEquals(200, new HttpClient("localhost", server.getPort(), "/hello")
+                .getStatusCode());
+        assertEquals(200, new HttpClient("localhost", server.getPort(), "/hello")
+                .getStatusCode());
+    }
+
+
+
+}

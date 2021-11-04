c800a0480dda012f915d1175fdad3d70ea7175d5
mojibhashemi123 <muha006@student.kristiania.no>
2021-11-04 20:50:28 +0100

    OptionToQnDao - should retrieve Option to question (passed)

--- AvansertJavaEksamen/src/main/java/no/kristiania/questionnaire/OptionToQnDao.java
+++ AvansertJavaEksamen/src/main/java/no/kristiania/questionnaire/OptionToQnDao.java
@@ -13,12 +13,48 @@ public class OptionToQnDao {
 
     public void save(OptionToQn optionToQn) throws SQLException {
 
+        try (Connection connection = dataSource.getConnection()) {
+            try (PreparedStatement statement = connection.prepareStatement(
+                    "insert into option_to_qn (option_value) values (?)",
+                    Statement.RETURN_GENERATED_KEYS
+            )) {
+                statement.setString(1, optionToQn.getOption());
+
+                statement.executeUpdate();
+
+                try (ResultSet rs = statement.getGeneratedKeys()) {
+                    rs.next();
+                    optionToQn.setId(rs.getLong("id"));
+                }
+            }
+        }
     }
 
 
     public OptionToQn retrieve(long id) throws SQLException {
-        return null;
+        try (Connection connection = dataSource.getConnection()) {
+            try (PreparedStatement statement = connection.prepareStatement(
+                    "select * from option_to_qn where id = ?"
+            )) {
+                statement.setLong(1, id);
+
+                try (ResultSet rs = statement.executeQuery()) {
+                    rs.next();
+
+                    return mapFromResultSet(rs);
+                }
+            }
+        }
     }
+
+    private OptionToQn mapFromResultSet(ResultSet rs) throws SQLException {
+        OptionToQn option = new OptionToQn();
+        option.setId(rs.getLong("id"));
+        option.setOption(rs.getString("option_value"));
+        return option;
+    }
+
+
 
 
 }

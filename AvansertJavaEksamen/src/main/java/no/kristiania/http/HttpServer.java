ca8ef15c13a6201831786db7adc0cee854a68ad3
mojibhashemi123 <muha006@student.kristiania.no>
2021-11-04 22:21:01 +0100

    Read dataSource from properties-file

--- AvansertJavaEksamen/src/main/java/no/kristiania/http/HttpServer.java
+++ AvansertJavaEksamen/src/main/java/no/kristiania/http/HttpServer.java
@@ -1,18 +1,19 @@
 package no.kristiania.http;
 
+import org.flywaydb.core.Flyway;
+import org.postgresql.ds.PGSimpleDataSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.sql.DataSource;
 import java.io.ByteArrayOutputStream;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class HttpServer {
 
@@ -136,7 +137,21 @@ public class HttpServer {
     public static void main(String[] args) throws IOException {
         HttpServer httpServer = new HttpServer(8000);
         logger.info("Starting http://localhost:{}/index.html", + httpServer.getPort());
+    }
 
+    private static DataSource createDataSource() throws IOException {
+
+        Properties prop = new Properties();
+        try (FileReader reader = new FileReader("pgr203.properties")) {
+            prop.load(reader);
+        }
+
+        PGSimpleDataSource dataSource = new PGSimpleDataSource();
+        dataSource.setURL(prop.getProperty("dataSource.url"));
+        dataSource.setUser(prop.getProperty("dataSource.username"));
+        dataSource.setPassword(prop.getProperty("dataSource.password"));
+        Flyway.configure().dataSource(dataSource).load().migrate();
+        return dataSource;
     }
 
     public int getPort() {

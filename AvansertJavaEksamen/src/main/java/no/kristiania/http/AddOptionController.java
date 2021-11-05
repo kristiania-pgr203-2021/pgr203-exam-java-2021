20266f637a207a706020e5e7eef83b44248002e2
mojibhashemi123 <muha006@student.kristiania.no>
2021-11-05 21:28:57 +0100

    HttpServer should echo query parameter(passed)

--- AvansertJavaEksamen/src/main/java/no/kristiania/http/AddOptionController.java
+++ AvansertJavaEksamen/src/main/java/no/kristiania/http/AddOptionController.java
@@ -6,13 +6,27 @@ import java.sql.SQLException;
 import java.util.Map;
 
 public class AddOptionController implements HttpController {
+    private final OptionToQnDao optionDao;
+
     public AddOptionController(OptionToQnDao optionDao) {
+        this.optionDao = optionDao;
     }
 
     @Override
     public HttpMessage handle(HttpMessage request) throws SQLException {
+        String[] requestLine = request.startLine.split(" ");
+        String requestTarget = requestLine[1];
 
+        int questionPos = requestTarget.indexOf('?');
+        String query = requestTarget.substring(questionPos+1);
 
-        return new HttpMessage("HTTP/1.1 200 Ok", "responseText");
+        String option = "";
+        if (query != null) {
+            Map<String, String> queryMap = HttpMessage.parseRequestParameters(query);
+            option = queryMap.get("option") + ", " + queryMap.get("questions");
+        }
+        String responseText = "<p>" + option + "</p>";
+
+        return new HttpMessage("HTTP/1.1 200 Ok", responseText);
     }
 }

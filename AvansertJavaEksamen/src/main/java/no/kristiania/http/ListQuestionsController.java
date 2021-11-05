b9cfe18433b28308cae627d8b885a560f275124d
mojibhashemi123 <muha006@student.kristiania.no>
2021-11-05 19:34:32 +0100

    HttpServer should list questions from database(passed)

--- AvansertJavaEksamen/src/main/java/no/kristiania/http/ListQuestionsController.java
+++ AvansertJavaEksamen/src/main/java/no/kristiania/http/ListQuestionsController.java
@@ -1,15 +1,26 @@
 package no.kristiania.http;
 
+import no.kristiania.questionnaire.Questionnaire;
 import no.kristiania.questionnaire.QuestionnaireDao;
 
 import java.sql.SQLException;
 
 public class ListQuestionsController implements HttpController {
+    private final QuestionnaireDao qreDao;
+
     public ListQuestionsController(QuestionnaireDao qreDao) {
+        this.qreDao = qreDao;
     }
 
     @Override
     public HttpMessage handle(HttpMessage request) throws SQLException {
-        return new HttpMessage("HTTP/1.1 200 Ok", "ok");
+        String response = "";
+
+        for (Questionnaire qre : qreDao.listAll()) {
+            response += "<div>" + qre.getQuestionTitle() + ", " + qre.getQuestionText() + "</div>";
+        }
+
+
+        return new HttpMessage("HTTP/1.1 200 Ok", response);
     }
 }
